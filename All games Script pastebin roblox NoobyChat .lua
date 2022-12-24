efda905467a4b95552e580d024d854d2f6687228
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:32:25 +0100

    commit zero

---
+++ All games Script pastebin roblox NoobyChat .lua
@@ -0,0 +1,15 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/97yQ5ZA7"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							You can make:
+  Fake system message
+  Fake friend join message
+  Fake server message
+More functions soon
+						
+]]
